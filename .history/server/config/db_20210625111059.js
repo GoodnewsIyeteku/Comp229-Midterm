19b265b1df6eb1c9645f8efeca13a6301e4ce12f
GoodnewsIyeteku <gd.iyeteku@yahoo.ca>
2021-06-25 12:18:47 -0400

    first commit

---
+++ .history/server/config/db_20210625111059.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://asa_neto:pass1@comp229asaneto.ohquv.mongodb.net/AsaNetoComp229?retryWrites=true&w=majority"
+};
---
+++ .history/server/config/db_20210625120011.js
@@ -0,0 +1,4 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://admin:password1234@cluster0.iz4x6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+};
